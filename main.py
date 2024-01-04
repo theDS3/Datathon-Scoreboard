328f0fb78da2c76275c61723eabb3d9e803e9e57
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2024-01-04 14:12:44 -0500

    feat: ✨ added fastapi server and organized code
    added an endpoint to update competitions list and number of teams

--- main.py
+++ main.py
@@ -1,86 +1,114 @@
-"""
-Installation, Imports, Variables
-"""
-# might have to pip install on the cloud
-# pip install kaggle
-# pip install pandas
-# pip install pymongo
 
-import os
 from zipfile import ZipFile
 from kaggle.api.kaggle_api_extended import KaggleApi
+from pymongo import MongoClient
+from datetime import datetime
+
+import os
 import pandas as pd
-import pymongo
-import datetime
 
-# set up environment variables (now we don't need kaggle.json)
-os.environ["KAGGLE_USERNAME"] = ""
-os.environ["KAGGLE_KEY"] = ""
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
 
-# Initialize and authenticate
-api = KaggleApi()
-api.authenticate()
+class Request(BaseModel):
+    competitions: list[str]
+    numOfTeams: int = 40
 
-competitions = ["llm-detect-ai-generated-text", "blood-vessel-segmentation", "UBC-OCEAN"]
-zip_path = os.path.join(os.getcwd(), "raw")
+app = FastAPI()
 
-"""
-Fetch leaderboards via Kaggle API
-"""
+ZIP_PATH = os.path.join(os.getcwd(), "zip")
+CSV_PATH = os.path.join(os.getcwd(), "csv")
 
-# Download zip files
-for comp in competitions:
-    api.competition_leaderboard_download(competition=comp, path=zip_path, quiet=True)
+def download_competitions(competitions: list[str]):
+    """ Downloads *.zip files containing leaderboard information from multiple competitions
 
-# Extract csv
+    Args:
+        competitions (list[str]): Competition IDs on Kaggle
+    """
 
-for item in os.listdir(zip_path):
-    if item.endswith(".zip"):
-        file_path = os.path.join(zip_path, item)
-        with ZipFile(file_path, "r") as zf:
-            target_path = os.path.join("CSV", os.path.splitext(item)[0] + ".csv")
-            source_path = zf.namelist()[0]
-            zf.getinfo(source_path).filename = target_path
-            zf.extract(source_path)
+    # Initialize and authenticate with Kaggle Public API
+    kaggle = KaggleApi()
+    kaggle.authenticate()
 
-"""
-Compute combined score
-"""
+    # Download zip files
+    for comp in competitions:
+        kaggle.competition_leaderboard_download(competition=comp, path=ZIP_PATH)
 
-# Read and combine csv's (assumes team names within each comp are unique and consistent across all comps)
-csv_path = os.path.join(os.getcwd(), "CSV")
-df = pd.DataFrame()
-for item in os.listdir(csv_path):
-    if item.endswith(".csv"):
-        file_path = os.path.join(csv_path, item)
-        df_comp = pd.read_csv(file_path)
-        df_comp = df_comp.filter(["TeamName", "Score"])
-        df_comp.columns = ["Name", "Score-"+item]
-        if (df.empty):
-            df = df_comp
-        else:
-            df = pd.merge(df, df_comp, on = ["Name"], how = "outer")
+def extract_competitions():
+    """ Extracts leaderboard information into *.csv from the *.zip files
+    """
 
-# compute combined score (placeholder)
-df["Score"] = df.drop(columns=["Name"]).sum(axis=1, skipna=True)
-df["Score"] = df["Score"].round(5)
-df.sort_values(by=['Score'], inplace=True, ascending=False)
-df = df.reset_index()
+    for item in os.listdir(ZIP_PATH):
+        if item.endswith(".zip"):
+            file_path = os.path.join(ZIP_PATH, item)
+            with ZipFile(file_path, "r") as zf:
+                target_path = os.path.join("csv", os.path.splitext(item)[0] + ".csv")
+                source_path = zf.namelist()[0]
+                zf.getinfo(source_path).filename = target_path
+                zf.extract(source_path)
 
-"""
-Export to Atlas
-"""
+def process_competitions(size: int) -> list:
+    """ Process the leaderboard data from multiple competitions
 
-# Format to array
-data = []
-for index, row in df.iterrows():
-    data.append({"Team": row["Name"], "Score": row["Score"]})
+    Args:
+        size (int, optional): Maximum entries for the leaderboard. Defaults to 40.
 
-# Send to cloud
-ATLAS_ENDPT = ""
-client = pymongo.MongoClient(ATLAS_ENDPT)
-db = client['dev']
-col = db['leaderboard']
-col.delete_many({})
-mydict = {"time": datetime.datetime.now(), "data": data}
-col.insert_one(mydict)
+    Returns:
+        list: Contains the name of the team and cumulative score
+    """
+
+    df = pd.DataFrame()
+    for item in os.listdir(CSV_PATH):
+        if item.endswith(".csv"):
+            file_path = os.path.join(CSV_PATH, item)
+            df_comp = pd.read_csv(file_path)
+            df_comp = df_comp.filter(["TeamName", "Score"])
+            df_comp.columns = ["Name", "Score-"+item]
+            df = df_comp if df.empty  else pd.merge(df, df_comp, on = ["Name"], how = "outer")
+
+    # compute combined score (placeholder)
+    df["Score"] = df.drop(columns=["Name"]).sum(axis=1, skipna=True)
+    df["Score"] = df["Score"].round(5)
+    df.sort_values(by=['Score'], inplace=True, ascending=False)
+    df = df.reset_index()
+
+    # Format to array
+    data = []
+    for _, row in df.head(size).iterrows():
+        data.append({"Team": row["Name"], "Score": row["Score"]})
+
+    return data
+
+@app.get("/" , status_code=status.HTTP_201_CREATED)
+def update_leaderboard(request: Request):
+    """ Updates Leaderboard with new scores
+
+    Args:
+        request (Request): JSON Request Body
+
+    Raises:
+        HTTPException: Missing Competition IDs
+
+    Returns:
+        JSON: Returns the message 'OK' if successfully else an Error
+    """
+
+    # Initialize and authenticate
+    client = MongoClient(os.getenv('MONGO_URI'))
+    db = client.dev
+
+    if request.competitions:
+        download_competitions(request.competitions)
+        extract_competitions()
+
+        db.leaderboard.insert_one(
+            {
+                "timestamp": datetime.now().strftime('%b %d %Y %I:%M %p'),
+                "data": process_competitions(request.numOfTeams)
+            }
+        )
+
+        # Return an HTTP response
+        return {"message": 'OK'}
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Competition IDs not provided")
